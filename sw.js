772ce5fc27f3294c1105764110dc4d1f6b5b0e13
criscard90 <110664371+criscard90@users.noreply.github.com>
2025-12-20 22:40:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
-self.addEventListener('fetch', (event) => {
-    // Anche se vuoto, la sua presenza abilita l'installazione su molti sistemi
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
+const CACHE_NAME = 'tv-flix-v1';
+const assets = ['index.html', 'app.js'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
     );
 });
